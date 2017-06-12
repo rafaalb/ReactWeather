958e358e1e70deeb1cfc02d9509da61ecef7471b
terra <rafaa.lb@hotmail.com>
2017-06-11 22:43:44 -0400

    added charts to weather

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,9 +1,28 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import Chart from '../components/chart';
 
 //export default class WeatherList extends Component{
 class WeatherList extends Component{ // we dont use the export default here because now we are using the connect function
+	renderWeather(cityData){
+		const name=cityData.city.name;
+		const temps=cityData.list.map(weather=>weather.main.temp);
+		const pressures=cityData.list.map(pressures=>pressures.main.temp);
+		const humidities=cityData.list.map(humidities=>humidities.main.humidity);
 
+		
+		console.log(temps);
+		return(
+			<tr key={name}>
+				<td>
+					{name}
+				</td>
+				<td><Chart data={temps} color="orange" /></td>
+				<td><Chart data={pressures} color="red" /></td>
+				<td><Chart data={humidities} color="blue" /></td>
+			</tr>
+			);
+	}
 	render(){
 		return(
 			<table className="table table-hover">
@@ -16,7 +35,7 @@ class WeatherList extends Component{ // we dont use the export default here beca
 					</tr>
 				</thead>
 				<tbody>
-
+					{this.props.weather.map(this.renderWeather)}
 				</tbody>
 			</table>
 			);
