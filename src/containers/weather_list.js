a1b730ff941395138d0c508e70ee428f05a0ac1b
Rafael Pina <rpina@solsteace.com>
2017-06-15 11:49:22 -0400

    added lodash function and finished graphs

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -7,19 +7,19 @@ class WeatherList extends Component{ // we dont use the export default here beca
 	renderWeather(cityData){
 		const name=cityData.city.name;
 		const temps=cityData.list.map(weather=>weather.main.temp);
-		const pressures=cityData.list.map(pressures=>pressures.main.temp);
+		const pressures=cityData.list.map(pressures=>pressures.main.pressure);
 		const humidities=cityData.list.map(humidities=>humidities.main.humidity);
 
-		
+
 		console.log(temps);
 		return(
 			<tr key={name}>
 				<td>
 					{name}
 				</td>
-				<td><Chart data={temps} color="orange" /></td>
-				<td><Chart data={pressures} color="red" /></td>
-				<td><Chart data={humidities} color="blue" /></td>
+				<td><Chart data={temps} color="orange" units="K"/></td>
+				<td><Chart data={pressures} color="red" units="hPa"/></td>
+				<td><Chart data={humidities} color="blue" units="%"/></td>
 			</tr>
 			);
 	}
@@ -29,9 +29,9 @@ class WeatherList extends Component{ // we dont use the export default here beca
 				<thead>
 					<tr>
 						<th>City</th>
-						<th>Temperature</th>
-						<th>Pressure</th>
-						<th>Humidity</th>
+						<th>Temperature (K)</th>
+						<th>Pressure(hPa)</th>
+						<th>Humidity(%)</th>
 					</tr>
 				</thead>
 				<tbody>
